bd26a0f179a33f8a64c146c5371a46b252fe96a7
Moein Mohammadi <moein.mohammadi69@gmail.com>
2025-12-20 09:54:08 +0330

    add chrome

--- file.pdf.js
+++ file.pdf.js
@@ -46,12 +46,15 @@ module.exports = {
 
 async function sendPdf(res, stringResult) {
 
-  const browser = await puppeteer.launch(
-    {
-      args: ['--no-sandbox']
-    }
-
-  );
+const browser = await puppeteer.launch({
+  headless: "new",
+  args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-dev-shm-usage",
+    "--disable-gpu",
+  ],
+});
   const page = await browser.newPage()
 
   // 2. Create PDF from static HTML
